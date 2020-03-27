4332f7a80dafa5ebd9a2b9b6eb233825f4291438
Allison Mackey <a.mackey693@gmail.com>
2020-03-27 11:17:55 -0700

    addresses bug in jQuery, need to run console.log() & debugger next.

--- js/scripts.js
+++ js/scripts.js
@@ -8,18 +8,30 @@ $(document).ready(function() {
     var favColor = parseInt($("#favColor").val());
     var result = (fridayNight + favActivity + favTvShow + favColor);
 
-    if (fidayNight === 0 || favActivity === 0 || favTvShow === 0 || favColor === 0) {
+    if (fridayNight === 0 || favActivity === 0 || favTvShow === 0 || favColor === 0) {
+      $(".blank").hide();
       $(".autoSelect").show();
       $(".autoSelect").attr("selected", "selected")
       $(".results").hide();
-    }
-    else if (result <=4) {
+    } else if (result <=4) {
       $(".results").show();
       $("#ruby").show();
       $("#go", "#java","#bianary").hide();
-    }
+    } else if (result <=8) {
+      $(".results").show();
+      $("#go").show();
+      $("#ruby", "#java","#bianary").hide();
+    } else if (result <=12) {
+      $(".results").show();
+      $("#java").show();
+      $("#ruby", "#go","#bianary").hide();
+    } else if (result > 13) 
+      $(".results").show();
+      $("#bianary").show();
+      $("#ruby", "#go", "#java").hide(); 
+    };
 
-    return (result)
+  return (result)
   })
 
 
