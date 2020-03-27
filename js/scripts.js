4c338d194b994d7ac7566444436f0c3d8bfff9e1
Allison Mackey <a.mackey693@gmail.com>
2020-03-27 10:51:13 -0700

    adds questions, options, links, & photos to HTML. adds additional classes & id's to css. adds initial jQuery functions & branches

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    event.preventDefault();
 
-  
+    var fridayNight = parseInt($("#fridayNight").val());
+    var favActivity = parseInt($("#favActivity").val());
+    var favTvShow = parseInt($("#favTvShow").val());
+    var favColor = parseInt($("#favColor").val());
+    var result = (fridayNight + favActivity + favTvShow + favColor);
+
+    if (fidayNight === 0 || favActivity === 0 || favTvShow === 0 || favColor === 0) {
+      $(".autoSelect").show();
+      $(".autoSelect").attr("selected", "selected")
+      $(".results").hide();
+    }
+    else if (result <=4) {
+      $(".results").show();
+      $("#ruby").show();
+      $("#go", "#java","#bianary").hide();
+    }
+
+    return (result)
+  })
 
 
 
